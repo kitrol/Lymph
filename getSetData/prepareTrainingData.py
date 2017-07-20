f6904bcf6a165c4003fc086c2b3417bbf14fa049
kitrol <234592380@qq.com>
2017-07-20 14:23:38 +0900

    计算均值

--- getSetData/prepareTrainingData.py
+++ getSetData/prepareTrainingData.py
@@ -12,6 +12,7 @@ import platform
 
 worktDir_ = '';
 TrainFolder = '\\..\\..\\train_data';
+TextFolder = '\\..\\..\\markedRegionsText\\';
 sysstr = platform.system();
 print(sysstr);
 if sysstr == "Windows":
@@ -49,26 +50,39 @@ def main(argv):
 	sumNum_R = 0.0;
 	sumNum_G = 0.0;
 	sumNum_B = 0.0;
-
-	sumNum_R += oriImg[0,0,0];
-	sumNum_G += oriImg[0,0,1];
-	sumNum_B += oriImg[0,0,3];
+	pixels = 0;
+	markedColors = [];
 
 	for width in range(0,oriImg.shape[0]):
 		for height in range(0,oriImg.shape[1]):
-			# print("pixel data is ",oriImg[width,height]);
+			pixels += 1;
+			sumNum_R = oriImg[width,height,0];
+			sumNum_G = oriImg[width,height,1];
+			sumNum_B = oriImg[width,height,2];
+			if (pixels==2):
+				sumNum_R = sumNum_R/2;
+				sumNum_G = sumNum_G/2;
+				sumNum_B = sumNum_B/2;
+				pass;
+			elif pixels>2:
+				sumNum_R = sumNum_R/2;
+				sumNum_G = sumNum_G/2;
+				sumNum_B = sumNum_B/2;
+				pass;
+
 			if (correctImg[width,height]==np.array([0,0,0])).all():
-				print(correctImg[width,height]);
+				# print(correctImg[width,height]);
 				regionsImage[width,height] = oriImg[width,height];
-				pass;
-			elif (oriImg[width,height]==correctImg[width,height]).all():
-				sumNum
-				
-				pass;
-			elif (correctImg[width,height]==np.array([0,0,0])).all():
-				pass;
+				markedColors.append((regionsImage[width,height,0],regionsImage[width,height,1],regionsImage[width,height,2])); #  output for average caculate
+	print("average color is R:%f  G:%f  B:%f \n" % (sumNum_R,sumNum_G,sumNum_B));
+	print(markedColors[:10]);
 
-
+	global TextFolder;
+	markedRegionColorFile = open( worktDir_+TextFolder+"markedRegionsText.txt", "wb");
+	for index in range(len(markedColors)):
+		string = "%d %d %d\\n"%(markedColors[index][0],markedColors[index][1],markedColors[index][2]);
+		markedRegionColorFile.write(string);
+	markedRegionColorFile.close();
 
 	cv.namedWindow('correctImg',cv.WINDOW_NORMAL);
 	cv.imshow('correctImg',correctImg);
